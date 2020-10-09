9d2cd6afcec15232d87c6f392448ffd33131df15
pjardel <AL1028138@tdf.hubtotal.net>
2020-10-09 18:11:52 +0200

    Add enumerate

--- molecular/molecule_parser.py
+++ molecular/molecule_parser.py
@@ -13,8 +13,17 @@ tag_dict = {
 
 
 def hello():
-
     logger.info(f'Hello {USERNAME}')
+    logger.info(f'len(character) = {len(USERNAME)}')
+    index_end = len(USERNAME)
+    for index, character in enumerate(USERNAME):
+        logger.info(f'index {index}')
+        if index < index_end:
+            index_next_char = index+1
+        else:
+            index_next_char = index_end-1
+        logger.info(f'index_next_char {index_next_char}')
+        logger.info(f'character = {index} {character}')
 
 
 if __name__ == '__main__':
